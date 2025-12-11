e997f667226ac7ddd2f78172559c44e165e6f8a8
Max <maxuberas@gmail.com>
2025-12-12 00:22:26 +0800

    [FEAT] Add pipe puzzle border, reset button, and  puzzle timer

--- src/features/portal/minigame/components/hud/StatusBar.tsx
+++ src/features/portal/minigame/components/hud/StatusBar.tsx
@@ -1,20 +1,40 @@
 import React from "react";
 import { Timer } from "../hud/Timer";
 import { Lives } from "../hud/Lives";
+import { Label } from "components/ui/Label";
 import giantBalls from "public/world/portal/images/GiantRedChristmasOrnament.webp";
+import { SUNNYSIDE } from "assets/sunnyside";
+import resetBtn from "public/world/portal/images/resetbutton_normal.webp";
+import { Button } from "components/ui/Button";
 
 export const StatusBar: React.FC = () => {
-    return (
-        <>
-            <div className="flex flex-row w-full flex-wrap items-center justify-center">
-               <div className="flex flex-row w-full justify-between pt-6 px-6 bg-[#265c42] rounded-t-[2.5rem] pb-4">
-                 <div className="flex flex-col gap-4">
-                   <Timer />
-                   <Lives />
-                 </div>
-                 <img className="w-[2rem] md:w-[3rem] h-full" src={giantBalls} />
-               </div>
-             </div>
-        </>
-    )
-}
+  return (
+    <>
+      <div className="flex flex-row w-full flex-wrap items-center justify-center">
+        <div className="flex flex-row w-full justify-between px-3 pt-6 md:px-6 bg-[#265c42] rounded-t-[2.5rem] pb-2">
+          <div className="flex flex-col gap-4">
+            <Timer />
+            <Lives />
+          </div>
+          <div className="flex flex-row gap-1 md:gap-3">
+            <Label icon={SUNNYSIDE.icons.timer} type="vibrant">
+              {/* Static - puzzle time */}
+              <span className="text-xs md:text-[2.5rem]">10</span>
+            </Label>
+            <Button>
+              <div className="flex items-center justify-center">
+                <img
+                  className="w-[2rem] md:w-[2.5rem] pt-[5px]"
+                  src={resetBtn}
+                  alt="reset button"
+                />
+                {/* Static - reset counter */}
+                <span className="text-xs md:text-[2.5rem] pl-2">3</span>
+              </div>
+            </Button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
